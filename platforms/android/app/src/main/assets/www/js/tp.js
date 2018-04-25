2eb1186257e852c0cfc9ecd69aeb13ef6678d636
Sharad Bharadia <bharadiasharad@gmail.com>
2018-04-26 00:05:37 +0530

    first commit

---
+++ platforms/android/app/src/main/assets/www/js/tp.js
@@ -0,0 +1,12 @@
+document.getElementById("vibration").addEventListener("click", vibration);
+document.getElementById("vibrationPattern").addEventListener("click", vibrationPattern);
+
+function vibration() {
+   var time = 3000;
+   navigator.vibrate(time);
+}
+
+function vibrationPattern() {
+   var pattern = [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100];
+   navigator.vibrate(pattern);
+}
